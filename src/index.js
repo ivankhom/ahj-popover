ce41633f11dea9c1f66e254b3909ec5824d26fad
ivankhom <156940438+ivankhom@users.noreply.github.com>
2026-05-29 16:34:14 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ const button = document.querySelector('.btn-popover');
 if (!button) throw new Error('Element .btn-popover not found');
 
 const controller = new PopoverController(button, {
-  title: 'Popover title',
-  content: "And here's some amazing content. It's very engaging. Right?",
+  title: button.dataset.title,
+  content: button.dataset.content,
 });
 
 controller.init();
